69897fdb9d30cc4d112a55cbc2e706c7ea6be3d0
MaichelYB <44602175+MaichelYB@users.noreply.github.com>
2019-03-30 16:17:38 +0700

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,28 @@
+
+
+<form name='frm' action='new 1.php' method='GET'>
+
+    <body>
+        Name:<input type='text' name='nama'><br><br>
+        Job:<input type='text' name='job'><br><br>
+        <button onclick=''>Submit</button><br><br>
+        <h1>People who submitted:</h1>
+        <div id='tableUser'>
+            <table border='1'>
+                <tr>
+                    <th>ID</th>
+                    <th>Nama</th>
+                    <th>Job</th>
+                    <th>Date</th>
+                </tr>
+            </table>
+        </div>
+        <script>
+            function submit() {
+
+            }
+        </script>
+
+    </body>
+</form>
+
